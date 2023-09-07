8ecf66f5310502fb8f6bdde750e91054e57e4f37
KrisKioko <chris.kioko20@gmail.com>
2023-09-07 12:36:07 +0300

    changes in app.js creating components

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Todo from "./components/Todo";
 
 
 function App(props) {
+  const taskList = props.tasks?.map((task) => task.name);
   return (
     <div className="todoapp stack-large">
       <h1>TodoMatic</h1>
@@ -58,7 +59,7 @@ function App(props) {
         role="list"
         className="todo-list stack-large stack-exception"
         aria-labelledby="list-heading">
-        <Todo name="Eat" completed={true} id="todo-0" />
+        <Todo name="Eat" completed={false} id="todo-0" />
         <Todo name="Sleep" completed={false} id="todo-1" />
         <Todo name="Repeat" completed={false} id="todo-2" />
       </ul>
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -2,17 +2,18 @@ import React from "react";
 
 
 function Todo(props) {
+  console.log(props.tasks)
     return (
       <li className="todo stack-small">
         <div className="c-cb">
           <input id={props.id} type="checkbox" defaultChecked={props.completed} />
-          <label className="todo-label" htmlFor="todo-0">
+          <label className="todo-label" htmlFor={props.id}>
             {props.name}
           </label>
         </div>
         <div className="btn-group">
           <button type="button" className="btn">
-            Edit <span className="vissually-hidden">{props.name}</span>
+            Edit <span className="visually-hidden">{props.name}</span>
           </button>
           <button type="button" className="btn btn__danger">
             Delete <span className="visually-hidden">{props.name}</span>
