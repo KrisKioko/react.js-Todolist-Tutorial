dff1bd3a542a67db0b048a76db5272c8ddef0bb1
KrisKioko <chris.kioko20@gmail.com>
2023-09-07 13:12:42 +0300

    rendering with iteration

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import Todo from "./components/Todo";
 
 
 function App(props) {
-  const taskList = props.tasks?.map((task) => task.name);
+  const taskList = props.tasks?.map((task) => <Todo id={task.id} name={task.name} completed={task.completed} key={task.id} />);
   return (
     <div className="todoapp stack-large">
       <h1>TodoMatic</h1>
@@ -59,9 +59,7 @@ function App(props) {
         role="list"
         className="todo-list stack-large stack-exception"
         aria-labelledby="list-heading">
-        <Todo name="Eat" completed={false} id="todo-0" />
-        <Todo name="Sleep" completed={false} id="todo-1" />
-        <Todo name="Repeat" completed={false} id="todo-2" />
+          {taskList}
       </ul>
     </div>
   );
